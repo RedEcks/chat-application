44b998a7a2201a17ea75884b6ca783fb88a80fb1
Xavier Dunkley <xavierxv18@gmail.com>
2023-08-08 19:00:55 -0400

    added sign in and sign up form

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,18 +3,42 @@ import Cookies from "universal-cookie";
 import axios from 'axios'
 
 import signinImage from '../assets/signup.jpg'
+const initialState = {
+    fullName:'',
+    username:'',
+    password:'',
+    confirmPassword:'',
+    phoneNumber:'',
+    avatarURL:'',
+}
+
 
 const Auth = ()=>{
     const [isSignup, setIsSignup] = useState(true)
+    const [form, setForm]=useState(initialState)
 
-    const handleChange = ()=>{}
+    const handleChange = (e)=>{
+        setForm({...form,[e.target.name]: e.target.value})
+
+        console.log(form)
+    }
+
+    const switchMode= ()=>{
+        setIsSignup((prevIsSignup)=>!prevIsSignup)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        console.log(form)
+    }
 
     return(
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     <p>{isSignup?'Sign Up': 'Sign In'}</p>
-                    <form onSubmit={()=>{}}>
+                    <form onSubmit={handleSubmit}>
                         {isSignup&&(
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="fullName">Full Name</label>
@@ -49,11 +73,55 @@ const Auth = ()=>{
                                 />
                             </div>
                         )}
+                        {isSignup&&(
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    name="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            {isSignup&&(
+                            <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="confirmpassword"> Confirm Password</label>
+                            <input
+                                name="confirmPassword"
+                                type="password"
+                                placeholder="Confirm Password"
+                                onChange={handleChange}
+                                required
+                            />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_button">
+                                <button>{isSignup? "Sign Up":"Sign In"}</button>
+                        </div>
                     </form>
+                    <div className="auth__form-container_fields-account">
+                                {isSignup?"Already have an account? "
+                                :"Don't have and account? "}
+                        <span onClick={switchMode}>{isSignup?' Sign In': ' Sign Up'}</span>
+                    </div>
                 </div>
 
             </div>
-
+                <div className="auth__form-container-image">
+                    <img src={signinImage} alt="Sign In" width={1000}/>
+                </div>
         </div>
     )
 }
