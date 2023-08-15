ef6e1575324202aee2978565ee235a230162c14a
Xavier Dunkley <xavierxv18@gmail.com>
2023-08-15 06:52:05 -0400

    started sign up in auth.js in server folder

--- auth.js
+++ auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const { signup, login } = require('../chat-app/server/auth')
+
 const router = express.Router()
 
-router.post('/signup')
-router.post('/login')
+router.post('/signup', signup)
+router.post('/login', login)
+
+module.exports = router
--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,29 @@
-const login =() => {}
-const signup =()=> {}
+const { connect } = require('getstream')
+const bcrypt = require('bcrypt')
+const StreamChat = require('stream-chat')
+const crypto = require('crypto') 
+
+
+const signup =(req,res)=> {
+    try{
+        const {fullName, username, password, phoneNumber} = req.body
+        const userId = crypto
+    }catch(error){
+        console.log(error)
+
+        res.status(500).json({message: errror})
+
+    }
+}
+const login =(req, res) => {
+    try{
+
+    }catch(error){
+        console.log(error)
+
+        res.status(500).json({message: errror})
+
+    }
+}
 
 module.exports = {signup, login}
